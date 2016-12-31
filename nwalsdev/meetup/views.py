c1bfd236994f9cfae88c1d87431043dcdeef7a50
Jacob Senecal <jacob.senecal@gmail.com>
2016-12-31 08:31:21 -0600

    Use template filter for upcoming meetups view and template

--- nwalsdev/meetup/views.py
+++ nwalsdev/meetup/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.utils import timezone
-from nwalsdev.meetup.models import Meetup
 
 def upcoming(request):
-    meetups = Meetup.objects.filter(start_time__gt=timezone.now())
-    return render(request, 'meetup/upcoming.html', {'meetups': meetups})
+    return render(request, 'meetup/upcoming.html')
