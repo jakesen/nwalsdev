e681f96b1b5f0c0b932788e110ab9d1a8dc5431d
Jacob Senecal <jacob.senecal@gmail.com>
2016-12-31 08:30:43 -0600

    Template filter for upcoming meetups

---
+++ nwalsdev/meetup/templatetags/meetup_tags.py
@@ -0,0 +1,14 @@
+from django import template
+from django.utils import timezone
+from datetime import timedelta
+from nwalsdev.meetup.models import Meetup
+
+
+register = template.Library()
+
+
+@register.filter
+def upcoming_meetups(user):
+    if user.is_authenticated:
+        two_hours = 2 * 60 * 60
+        return Meetup.objects.filter(start_time__gt=timezone.now()+timedelta(seconds=two_hours))
